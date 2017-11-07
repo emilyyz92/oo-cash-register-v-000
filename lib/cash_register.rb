6c19590b4f7cf8f81f0da3f989065d75bef545e1
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-07 01:27:19 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
+  attr_accessor :cash_register
+  attr_reader :cash_register_with_discount
+  attr_writer :total
 
+
+  def initialize(discount = 20)
+    self.cash_register_with_discount = discount
+    self.total = 0
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title,price,quantity = 1)
+    self.total = self.total + price*quantity
+  end
+
+  def apply_discount(title,price)
+    price = price * 0.8
+    puts "After the discount, the total comes to #{price}."
+    self.total = self.total - price*0.2
+  end
+end

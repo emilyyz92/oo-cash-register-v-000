e904930d280d8de5b3b906e99a9c3d412988b8b9
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-07 02:18:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   attr_accessor :cash_register, :title, :price, :last_total, :last_title
-  attr_reader :cash_register_with_discount
+  attr_reader :discount
   attr_writer :total, :items
 
 
